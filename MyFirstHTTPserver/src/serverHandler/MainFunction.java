b5e45a4b8dba18297760963001ef87bc6918d2de
thomashed <thomashj93@gmail.com>
2014-08-29 18:39:23 +0200

    Made project and class MainFunction

---
+++ MyFirstHTTPserver/src/serverHandler/MainFunction.java
@@ -0,0 +1,22 @@
+package serverHandler;
+import java.io.*;
+import java.net.*;
+import com.sun.net.httpserver.*;
+import java.net.InetSocketAddress;
+
+public class MainFunction {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
