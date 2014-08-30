510862ab5fcf0ce057814a6813d1028a47c34c94
thomashed <thomashj93@gmail.com>
2014-08-30 12:01:27 +0200

    Updated MainFunction
    - Added handler and server.methods()

--- MyFirstHTTPserver/src/serverHandler/MainFunction.java
+++ MyFirstHTTPserver/src/serverHandler/MainFunction.java
@@ -1,22 +1,30 @@
 package serverHandler;
+
 import java.io.*;
 import java.net.*;
 import com.sun.net.httpserver.*;
-import java.net.InetSocketAddress;
 
 public class MainFunction {
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    public static void main(String[] args) throws Exception {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(InetAddress.getLoopbackAddress(), 80), 0);
+        server.createContext("/", new indexHandler()); // Now the handler is attached to root
+        server.start();
+        
+    }
+
+    static class indexHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+            String response = "Hello World!";
+            he.sendResponseHeaders(200, response.length());
+            try (OutputStream os = he.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        }
+
+    }
+
 }
